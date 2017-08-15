f7e2f2c40461007704175f384c42cf520eca6b7d
Colin Pok <hughpok@gmail.com>
2017-08-15 17:06:28 +0700

    Update ScrollableTabBar.js

--- ScrollableTabBar.js
+++ ScrollableTabBar.js
@@ -11,25 +11,26 @@ const {
   I18nManager
 } = ReactNative;
 const Button = require('./Button');
-
+var createReactClass = require('create-react-class');
+import PropTypes from 'prop-types'
 const WINDOW_WIDTH = Dimensions.get('window').width;
 
-const ScrollableTabBar = React.createClass({
+const ScrollableTabBar = createReactClass({
   propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array,
-    backgroundColor: React.PropTypes.string,
-    activeTextColor: React.PropTypes.string,
-    inactiveTextColor: React.PropTypes.string,
-    scrollOffset: React.PropTypes.number,
+    goToPage: PropTypes.func,
+    activeTab: PropTypes.number,
+    tabs: PropTypes.array,
+    backgroundColor: PropTypes.string,
+    activeTextColor: PropTypes.string,
+    inactiveTextColor: PropTypes.string,
+    scrollOffset: PropTypes.number,
     style: ViewPropTypes.style,
     tabStyle: ViewPropTypes.style,
     tabsContainerStyle: ViewPropTypes.style,
-    textStyle: Text.propTypes.style,
-    renderTab: React.PropTypes.func,
+    textStyle: propTypes.style,
+    renderTab: PropTypes.func,
     underlineStyle: ViewPropTypes.style,
-    onScroll:React.PropTypes.func,
+    onScroll: PropTypes.func,
   },
 
   getDefaultProps() {
