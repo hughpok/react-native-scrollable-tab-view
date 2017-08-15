5b74185714535be792e8a4fc95199dd19c990132
Colin Pok <hughpok@gmail.com>
2017-08-15 17:05:11 +0700

    Update DefaultTabBar.js

--- DefaultTabBar.js
+++ DefaultTabBar.js
@@ -1,4 +1,6 @@
 const React = require('react');
+var createReactClass = require('create-react-class');
+import PropTypes from 'prop-types'
 const { ViewPropTypes } = ReactNative = require('react-native');
 const {
   StyleSheet,
@@ -8,17 +10,17 @@ const {
 } = ReactNative;
 const Button = require('./Button');
 
-const DefaultTabBar = React.createClass({
+const DefaultTabBar = createReactClass({
   propTypes: {
-    goToPage: React.PropTypes.func,
-    activeTab: React.PropTypes.number,
-    tabs: React.PropTypes.array,
-    backgroundColor: React.PropTypes.string,
-    activeTextColor: React.PropTypes.string,
-    inactiveTextColor: React.PropTypes.string,
+    goToPage: PropTypes.func,
+    activeTab: PropTypes.number,
+    tabs: PropTypes.array,
+    backgroundColor: PropTypes.string,
+    activeTextColor: PropTypes.string,
+    inactiveTextColor: PropTypes.string,
     textStyle: Text.propTypes.style,
     tabStyle: ViewPropTypes.style,
-    renderTab: React.PropTypes.func,
+    renderTab: PropTypes.func,
     underlineStyle: ViewPropTypes.style,
   },
 
